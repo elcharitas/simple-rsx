ac34cf2af629f412d636853e3ae308228004f934
elcharitas <jonathanirhodia@gmail.com>
2025-05-26 14:54:59 +0100

    refactor: remove unused wasm console logging bindings

--- simple-rsx/src/dom.rs
+++ simple-rsx/src/dom.rs
@@ -6,33 +6,6 @@ pub trait WasmRender {
 }
 
 #[cfg(feature = "wasm")]
-#[wasm_bindgen::prelude::wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    pub fn log(s: &str);
-
-    // The `console.log` is quite polymorphic, so we can bind it with multiple
-    // signatures. Note that we need to use `js_name` to ensure we always call
-    // `log` in JS.
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_u32(a: u32);
-
-    // Multiple arguments too!
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_many(a: &str, b: &str);
-}
-
-#[cfg(feature = "wasm")]
-#[macro_export]
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => ($crate::dom::log(&format_args!($($t)*).to_string()))
-}
-
-#[cfg(feature = "wasm")]
 impl WasmRender for crate::Element {
     fn render(&self, mount: &web_sys::Element) -> Option<web_sys::Element> {
         let element = web_sys::window()
